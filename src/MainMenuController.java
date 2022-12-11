f2095ef99c2ff3cc7b7774834c2a9a855939edf6
Urdhanaka Aptanagi <46347836+urdhanaka@users.noreply.github.com>
2022-12-11 10:34:35 +0700

    fix: redesign main menu

--- src/MainMenuController.java
+++ src/MainMenuController.java
@@ -5,21 +5,32 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 public class MainMenuController {
 
     @FXML
-    private ImageView cover_img;
-
-    @FXML
-    void changeScene(ActionEvent event) throws IOException {
+    void changeSceneToGameStart(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("StartGame.fxml"));
         Stage window = (Stage)(((Node) event.getSource()).getScene().getWindow());
         window.setTitle("Hangman");
         window.setScene(new Scene(root, 800, 650));
         window.show();
+    }
+
+    @FXML
+    void changeSceneToHighScore(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("HighScore.fxml"));
+        Stage window = (Stage)(((Node) event.getSource()).getScene().getWindow());
+        window.setTitle("Hangman");
+        window.setScene(new Scene(root, 800, 650));
+        window.show();
+    }
+    
+    @FXML
+    void exitGame(ActionEvent event) throws IOException {
+        Stage window = (Stage)(((Node) event.getSource()).getScene().getWindow());
+        window.close();
     }
 }
